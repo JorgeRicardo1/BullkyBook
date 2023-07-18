9861021b21d792b1665829fc48be29659ea9a1b6
Jorge <jorge.ricardo@utp.edu.co>
2023-07-18 18:01:21 -0500

    Realizadas todas las configuraciones iniciales, estilos y terminada la interfaz grafica

---
+++ BullkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BullkyBookWeb.Data;
+using BullkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoriesList = _db.Categories;
+            return View(objCategoriesList);
+        }
+
+        
+    }
+}
