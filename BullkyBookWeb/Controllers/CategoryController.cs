bac32150686cb7049ba03457fbcab5abc1a08bca
Jorge <jorge.ricardo@utp.edu.co>
2023-07-19 13:27:14 -0500

    Funcion Add agregada

--- BullkyBookWeb/Controllers/CategoryController.cs
+++ BullkyBookWeb/Controllers/CategoryController.cs
@@ -19,6 +19,30 @@ namespace BullkyBookWeb.Controllers
             return View(objCategoriesList);
         }
 
-        
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display order cannot match the name");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
